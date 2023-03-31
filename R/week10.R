663d12fa4d2b3b3e4d421f5890bb0c8249c2e069
Reed W Priest <pries153@umn.edu>
2023-03-31 10:42:02 -0500

    Made code to split data into training and test sets.

--- R/week10.R
+++ R/week10.R
@@ -2,6 +2,7 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(haven)
+library(caret)
 set.seed(123)
 
 ## Data Import and Cleaning
@@ -10,5 +11,18 @@ gss_tbl <- read_spss("../data/GSS2016.sav") %>%
   drop_na(HRS2) %>%
   select_if(~(mean(is.na(.)) * 100) <= 75) # This line combines several verbs on the same line, but only for added readability.
 
-# Now I need to remove all IDKs and not clearly answered items for all columns.
+## Visualization
 
+(ggplot(gss_tbl, aes(x = HRS2)) +
+  geom_histogram(binwidth = 10) +
+  labs(x = "Workhours", y = "Count")) %>%
+  ggsave("../figs/fig1.png", ., width = 1920, height = 1080, units = "px")
+
+## Analysis
+gss_random_tbl <- gss_tbl[sample(nrow(gss_tbl)), ]
+gss_random75_tbl <- round(nrow(gss_random_tbl) * 0.75, 0)
+gss_train_tbl <- gss_random_tbl[1:gss_random75_tbl, ]
+kfolds <- createFolds(gss_train_tbl$HRS2, 10)
+gss_test_tbl <- gss_random_tbl[(gss_random75_tbl + 1):nrow(gss_random_tbl), ]
+
+#Now use this setup to create the four models using a similar formula, but unique method = "" settings.
