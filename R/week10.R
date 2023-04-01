5a07af19bce0710e548950f1683d37c052eb9e0a
Reed W Priest <pries153@umn.edu>
2023-03-31 20:09:45 -0500

    Identified correct criterion (HRS1 instead of HRS2).

--- R/week10.R
+++ R/week10.R
@@ -8,21 +8,21 @@ set.seed(123)
 ## Data Import and Cleaning
 gss_tbl <- read_spss("../data/GSS2016.sav") %>%
   as_tibble() %>%
-  drop_na(HRS2) %>%
+  drop_na(HRS1) %>%
   select_if(~(mean(is.na(.)) * 100) <= 75) # This line combines several verbs on the same line, but only for added readability.
 
 ## Visualization
 
-(ggplot(gss_tbl, aes(x = HRS2)) +
+(ggplot(gss_tbl, aes(x = HRS1)) +
   geom_histogram(binwidth = 10) +
   labs(x = "Workhours", y = "Count")) %>%
   ggsave("../figs/fig1.png", ., width = 1920, height = 1080, units = "px")
 
 ## Analysis
 gss_random_tbl <- gss_tbl[sample(nrow(gss_tbl)), ]
-gss_random75_tbl <- round(nrow(gss_random_tbl) * 0.75, 0)
+gss_random75 <- round(nrow(gss_random_tbl) * 0.75, 0)
 gss_train_tbl <- gss_random_tbl[1:gss_random75_tbl, ]
-kfolds <- createFolds(gss_train_tbl$HRS2, 10)
+kfolds <- createFolds(gss_train_tbl$HRS1, 10)
 gss_test_tbl <- gss_random_tbl[(gss_random75_tbl + 1):nrow(gss_random_tbl), ]
 
 #Now use this setup to create the four models using a similar formula, but unique method = "" settings.
